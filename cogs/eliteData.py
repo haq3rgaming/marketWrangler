abac323e0b1d8483ae10f667b1a71f4485697682
Hacker869 <puskarpato@gmail.com>
2023-03-27 19:09:34 +0200

    alerts

--- cogs/eliteData.py
+++ cogs/eliteData.py
@@ -65,6 +65,10 @@ class database(object):
         self.database[key].append(value)
         self.save()
     
+    def appendDict(self, key: str, dictKey: str, dictValues: dict) -> None:
+        self.database[key][dictKey] = dictValues
+        self.save()
+    
     def save(self, reopen: bool = True) -> None:
         self.databaseObject = open(self.databasePath, "w")
         json.dump(self.database, self.databaseObject, indent=4)
@@ -112,27 +116,29 @@ class commodityTableSelect(Select):
             await interaction.edit_original_response(content=f"```{dataTable}\n  Data updated at: {updateTime}```", embed=None, view=None)
 
 class commodityGraphView(View):
-    def __init__(self, createGraphFromCommodityData):
+    def __init__(self, createGraphFromCommodityData, hours: int = 24):
         super().__init__()
         self.add_item(commodityGraphSelect(
             options=selectOptions,
             placeholder="Select a commodity",
             min_values=1,
             max_values=len(selectOptions),
-            createGraphFromCommodityData = createGraphFromCommodityData
+            createGraphFromCommodityData = createGraphFromCommodityData,
+            hours = hours
         ))
 
 class commodityGraphSelect(Select):
-    def __init__(self, createGraphFromCommodityData, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, options, placeholder, min_values, max_values, createGraphFromCommodityData, hours):
+        super().__init__(options=options, placeholder=placeholder, min_values=min_values, max_values=max_values)
         self.callback = self.graphSelectCallback
         self.createGraphFromCommodityData = createGraphFromCommodityData
+        self.hours = hours
     
     async def graphSelectCallback(self, interaction: discord.Interaction) -> None:
         await interaction.response.defer()
         selectedResources = self.values
         await interaction.edit_original_response(content="Generating graph...", embed=None, view=None)
-        graphImage = self.createGraphFromCommodityData(selectedResources)
+        graphImage = self.createGraphFromCommodityData(selectedResources, self.hours)
         graphFile = discord.File(graphImage, filename="graph.png")
         await interaction.edit_original_response(content=None, embed=None, view=None, attachments=[graphFile])
 
@@ -144,6 +150,9 @@ class alertMessageView(View):
 
     @discord.ui.button(label="Keep alert", style=discord.ButtonStyle.gray)
     async def keepAlert(self, interaction: discord.Interaction, button: discord.ui.Button):
+        guildAlerts = self.alertDatabase.get(str(interaction.guild.id))
+        del guildAlerts[self.commodityID]
+        self.alertDatabase.set(str(interaction.guild.id), guildAlerts)
         await interaction.response.edit_message(view=None)
 
     @discord.ui.button(label="Dismiss", style=discord.ButtonStyle.gray)
@@ -216,7 +225,7 @@ class eliteData(commands.Cog):
         )
         return dataTable
 
-    def createGraphFromCommodityData(self: commands.Cog, commodities: list[str]) -> io.BytesIO:
+    def createGraphFromCommodityData(self: commands.Cog, commodities: list[str], hours: int = 24) -> io.BytesIO:
         fig = plt.figure(clear=True, figsize=(7.5, 5), dpi=100)
         ax = fig.gca()
         ax.grid(True)
@@ -231,7 +240,7 @@ class eliteData(commands.Cog):
         unixTimeOfData = []
 
         for id in commodities:
-            latestData = self.mainDatabase.getLatestByTime(id, 24)
+            latestData = self.mainDatabase.getLatestByTime(id, hours)
             x, y = [], []
             
             for record in latestData:
@@ -313,18 +322,42 @@ class eliteData(commands.Cog):
         await interaction.delete_original_response()
 
     @app_commands.command(name="graph", description="Shows the graph of a commodity/commodities")
-    async def graph(self: commands.Cog, interaction: discord.Interaction) -> None:
+    async def graph(self: commands.Cog, interaction: discord.Interaction, hours: int = 24) -> None:
         await interaction.response.defer()
         embed = discord.Embed(title="Select commodities you want data from:", color=0x00ff00)
-        await interaction.edit_original_response(embed=embed, view=commodityGraphView(self.createGraphFromCommodityData))
+        await interaction.edit_original_response(embed=embed, view=commodityGraphView(createGraphFromCommodityData=self.createGraphFromCommodityData, hours=hours))
 
     @app_commands.command(name="alert", description="Sets an alert for a commodity")
     @app_commands.choices(commodity=alertOptions)
     async def alert(self: commands.Cog, interaction: discord.Interaction, commodity: str, price: int, role: discord.Role) -> None:
         await interaction.response.defer()
-        if not self.alertDatabase.exists(str(interaction.guild.id)): self.alertDatabase.set(str(interaction.guild.id), {})
-        self.alertDatabase.set(str(interaction.guild.id), {commodity: {"alertPrice": price, "alertRoleID": role.id, "alertUserID": interaction.user.id}})
+        if not self.alertDatabase.exists(str(interaction.guild.id)): self.alertDatabase.set(str(interaction.guild.id), {}); print("new guild")
+        self.alertDatabase.appendDict(str(interaction.guild.id), commodity, {"alertPrice": price, "alertRoleID": role.id, "alertUserID": interaction.user.id})
         await interaction.edit_original_response(content=f"Alert set for {aviableCommodities[commodity]} at {'{:,}'.format(price)} Cr!", embed=None, view=None)
+    
+    @app_commands.command(name="removealert", description="Removes an alert for a commodity")
+    @app_commands.choices(commodity=alertOptions)
+    async def removeAlert(self: commands.Cog, interaction: discord.Interaction, commodity: str) -> None:
+        guildAlerts = self.alertDatabase.get(str(interaction.guild.id))
+        if commodity in guildAlerts.keys():
+            alertInfo = guildAlerts[commodity]
+            del guildAlerts[commodity]
+            self.alertDatabase.set(str(interaction.guild.id), guildAlerts)
+            interaction.edit_original_response(content=f"Alert at {alertInfo['alertPrice']} removed for {aviableCommodities[commodity]}!", embed=None, view=None)
+        else:
+            interaction.edit_original_response(content=f"No alert set for {aviableCommodities[commodity]}!", embed=None, view=None)
+    
+    @app_commands.command(name="alerts", description="Shows all alerts for the guild")
+    async def alerts(self: commands.Cog, interaction: discord.Interaction) -> None:
+        await interaction.response.defer()
+        if not self.alertDatabase.exists(str(interaction.guild.id)): self.alertDatabase.set(str(interaction.guild.id), {})
+        guildAlerts = self.alertDatabase.get(str(interaction.guild.id))
+        if guildAlerts == {}: await interaction.edit_original_response(content="No alerts set!", embed=None, view=None)
+        else:
+            embed = discord.Embed(title="Alerts:", color=0x00ff00)
+            for commodity in guildAlerts.keys():
+                embed.add_field(name=aviableCommodities[commodity], value=f"Price: {'{:,}'.format(guildAlerts[commodity]['alertPrice'])} Cr\nRole: <@&{guildAlerts[commodity]['alertRoleID']}>\nSet by ", inline=False)
+            await interaction.edit_original_response(content=None, embed=embed, view=None)
 
     @tasks.loop(hours=1, reconnect=True)
     async def updateCommodityDatabase(self: commands.Cog) -> None:
